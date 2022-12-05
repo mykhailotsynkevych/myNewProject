386cbb3414f0da789f96f1d402f24709034efd5b
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-12-05 19:30:38 +0100

    Update CreatePostsScreen.jsx

--- Screens/mainScreen/CreatePostsScreen.jsx
+++ Screens/mainScreen/CreatePostsScreen.jsx
@@ -55,6 +55,7 @@ const CreatePostsScreen = ({ navigation }) => {
       };
 
       setstate((prevState) => ({ ...prevState, coordinate: coords }));
+      console.log("Hi");
     })();
   }, []);
 
